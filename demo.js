08f6016d55bc3e4a0bf6f3235f6c85df42ac0ef7
pranavprathyu <pranavrono007@gmail.com>
2024-07-29 11:27:52 +0530

    Array

---
+++ demo.js
@@ -0,0 +1,288 @@
+// let num =20.5
+// console.log(typeof(num));
+
+
+ 
+ 
+
+
+
+// a=10
+// b=20 
+
+
+//  c=a
+//  a=b
+//  b=c
+
+//  console.log("a is",a);
+//  console.log("b is ",b);
+
+//  a=30-10
+//  b=30-20
+
+//  console.log("a is",30-10);
+//  console.log("b is",30-20);
+
+
+// let a=10
+// let b=20
+
+// a = a+b
+// console.log(a);
+// b= a-b
+// console.log(b);
+// a= a-b
+// console.log(a);
+    
+
+
+
+
+// a=20 b=10
+
+
+// console.log(a);
+
+
+
+
+
+
+// let a =20
+// let b = 30
+
+// let c =a+b
+
+
+// document.getElementById('hello').innerHTML = c
+// document.write(c)
+// alert(c)
+// console.log(c);
+
+// let a = ['asdasd','asdasd','asdasdasd']
+
+// console.log(a[a.length-1]);
+
+// let a = 20
+// let  b =20
+
+
+// if(a==="20" || b===20){
+//     console.log(true);
+// // }else{
+// //     console.log(false);
+// // }
+
+// // let c = a+b
+// // console.log(c);
+
+// // c = a-b
+// // console.log(c);
+
+// // c=a*b
+// // console.log(c);
+
+// // c=a**b
+// // console.log(c);
+
+// let a = 20
+// let  b =20
+
+// c=a&&=b
+// console.log(c);
+
+// c=a==b
+// console.log(c);
+
+// c=a===b
+// console.log(c);
+
+// c=a!=b
+// console.log(c);
+
+// c=a>b
+// console.log(c);
+
+// c=a>=b
+// console.log(c);
+
+// c=a<=b
+// console.log(c);
+
+// c=a+=b
+// console.log(c);
+
+// a<<=b
+// console.log(c);
+
+
+// let num=10
+
+
+// // if(num==10){
+// //   console.log("true");
+// // }else{
+// //   console.log(false);
+// // }
+
+// let data = num==10?true:false
+// console.log(data);
+
+// let num1=10
+// let num2 = 20
+// if(num1>num2){
+//     console.log(`${num1} is greater `);
+// }
+// else {
+//     console.log(`${num2} is greater`);
+// }
+
+// let num1=10
+// let num2=40
+// let num3=30
+// if(num1>num2){
+//     console.log(`${num1} is greater`);
+// }
+//    else if(num2>num3) {
+//     console.log(`${num2} is greater`);
+//    } 
+//    else{
+//     console.log(`${num3} is greater`);
+//    }
+
+
+// let num1=0
+// let num2=20
+// let num3=30
+
+//    if(num1 > 20 )
+//     {
+//         console.log("Number is NEGATIVE");
+//     }
+//     if(num1 < 20 )
+//     {
+//         console.log("Number is POSITIVE");
+//     }
+//     if(num1 == 0)
+//     {
+//         console.log("Number is ZERO");
+//     }
+
+    
+
+  
+// let a = ["h","e","l","l","o","w","o","r","l","d"];
+// let a = "helloworld"
+// let a = [1,23,4,4,6]
+// let target=6
+
+// let  sum =0
+// for (let i = 0; i < a.length; i++) {
+//   if
+//  sum+= a[i]
+// }
+// console.log(sum);
+
+// let a = [1,23,4,4,6]
+// let target=6
+//  for (let i = 0; i < a.length; i++) {
+//   if (a[i]===target)
+//     console.log(true);
+//   }
+
+// else  (a{i}=1)
+//   console.log(false);
+//  }
+
+
+// let person = {
+//   name1:"hello",
+//   age:23,
+//   gender:"m"
+// }
+
+
+
+// for(i=0;i<=10;i++){
+//   if(i%2==0){
+//     console.log(i);
+//   }
+// }
+
+// for(i=1; i<=10; i++){
+//   if(i%2!=0){
+//     console.log(i);
+//   }
+// }
+
+
+// let a = [1,2,3,4,6,34]
+
+
+// for(i=0;i<a.length;i++){
+//   if(a[i]%2==0){
+//     console.log(a[i]);
+//   }
+// }
+
+// let a =[5,18,23,25,53,64,29,12,19]
+
+//  for(i=0;i<a.length;i++){
+//   if(a[i]%2==0){
+//     console.log(a[i]);
+//   }
+//  }
+
+// let a =`"'alksjdjas'"`
+
+//  let a = 3
+//  for(i=1;i<=10;i++){
+
+//     console.log(`${a} X ${i} = ${a*i}`);
+  
+//  }
+
+
+// function hello(a){
+// return a
+// }
+
+
+
+// let c =hello("hello")
+// console.log(c);
+
+// function hello(a,b){
+//    let c=a+b
+//    console.log(c);
+// }
+// hello(1,2)
+// hello(3,4)
+// hello(8,9)
+
+// function hello(a,b){
+//    return a+b
+// }
+
+// console.log(hello(2,3));
+
+// const name =[11,23,4,55,4]
+// console.log(name[1],name[2],name[4]);
+// let n = name.length
+// name[2]='kajsdk'
+// console.log(name);
+// console.log(n);
+// console.log(name[n-2]);
+
+
+// for(i=0;i<n;i++){
+//   console.log(name[i]);
+// }
+
+const name =[11,23,4,55,32,8]
+let n =name.length
+for(i=0;i<n;i++){
+  if(name[i]%2==0)
+  console.log(name[i]);
+}
